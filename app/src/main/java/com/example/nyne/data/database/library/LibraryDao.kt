118af3cad1d001633991989c4822f9f1235ef33c
CherifiMi <cherifi154949@gmail.com>
2023-08-10 11:28:14 +0100

    library dao

--- app/src/main/java/com/example/nyne/data/database/library/LibraryDao.kt
+++ app/src/main/java/com/example/nyne/data/database/library/LibraryDao.kt
@@ -1,4 +1,22 @@
 package com.example.nyne.data.database.library
 
+import androidx.lifecycle.LiveData
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
 class LibraryDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(libraryItem: LibraryItem)
+
+    @Delete
+    fun delete(libraryItem: LibraryItem)
+
+    @Query("SELECT * FROM book_library ORDER BY id ASC")
+    fun getAllItems(): LiveData<List<LibraryItem>>
+
+    @Query("SELECT * FROM book_library WHERE book_id = :bookId")
+    fun getItemById(bookId: Int): LibraryItem?
+
 }
