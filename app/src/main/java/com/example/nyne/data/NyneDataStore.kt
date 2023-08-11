8e9e2c5b0c105cf13495c4cdcf890ab3c62d5584
CherifiMi <cherifi154949@gmail.com>
2023-08-11 01:19:15 +0100

    .

--- app/src/main/java/com/example/nyne/data/NyneDataStore.kt
+++ app/src/main/java/com/example/nyne/data/NyneDataStore.kt
@@ -4,10 +4,39 @@ import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.preferencesDataStore
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.emptyPreferences
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import java.io.IOException
 
 val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "on_boarding_pref")
 class NyneDataStore(context: Context) {
+    private val dataStore = context.dataStore
 
+    private object PreferencesKey {
+        val onBoardingKey = booleanPreferencesKey(name = "on_boarding_completed")
+    }
 
+    suspend fun saveOnBoardingState(completed: Boolean) {
+        dataStore.edit { preferences ->
+            preferences[PreferencesKey.onBoardingKey] = completed
+        }
+    }
+
+    fun readOnBoardingState(): Flow<Boolean> {
+        return dataStore.data.catch { err ->
+            if (err is IOException){
+                emit(emptyPreferences())
+            }else{
+                throw err
+            }
+        }.map {preferences ->
+            val onboardingState = preferences[PreferencesKey.onBoardingKey] ?: false
+            onboardingState
+        }
+    }
 
 }
