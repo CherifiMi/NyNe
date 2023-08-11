89f99ac66979d829111eb24a8660d294531b6226
CherifiMi <cherifi154949@gmail.com>
2023-08-11 01:15:09 +0100

    .

--- app/src/main/java/com/example/nyne/data/NyneDataStore.kt
+++ app/src/main/java/com/example/nyne/data/NyneDataStore.kt
@@ -1,7 +1,13 @@
 package com.example.nyne.data
 
 import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.preferencesDataStore
+import androidx.datastore.preferences.core.Preferences
 
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "on_boarding_pref")
 class NyneDataStore(context: Context) {
-    // TODO: fill
+
+
+
 }
