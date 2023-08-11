ecb477250268298baf8156301d074254acff7bcb
CherifiMi <cherifi154949@gmail.com>
2023-08-11 01:12:14 +0100

    .

---
+++ app/src/main/java/com/example/nyne/util/others/Paginator.kt
@@ -0,0 +1,31 @@
+package com.example.nyne.util.others
+
+class Paginator<Page, BookSet>(
+    private val initialPage: Page,
+    private inline val onLoadUpdated: (Boolean) -> Unit,
+    private inline val onRequest: suspend (nextPage: Page) -> Result<BookSet>,
+    private inline val getNextPage: suspend (BookSet) -> Page,
+    private inline val onError: suspend (Throwable?) -> Unit,
+    private inline val onSuccess: suspend (item: BookSet, newPage: Page) -> Unit
+) {
+    private var currentPage = initialPage
+    private var isMaskingRequest = false
+
+    suspend fun loadNextItems(){
+        if (isMaskingRequest){
+            return
+        }
+        isMaskingRequest = true
+        onLoadUpdated(true)
+        val result = onRequest(currentPage)
+        isMaskingRequest = false
+        val bookSet = result.getOrElse {
+            onError(it)
+            onLoadUpdated(false)
+            return
+        }
+        currentPage = getNextPage(bookSet)
+        onSuccess(bookSet, currentPage)
+        onLoadUpdated(false)
+    }
+}
