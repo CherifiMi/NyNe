1ee8142f74627eb6715cb716f0706846a69ae6d7
CherifiMi <cherifi154949@gmail.com>
2023-08-12 10:05:27 +0100

    .

--- app/src/main/java/com/example/nyne/MainActivity.kt
+++ app/src/main/java/com/example/nyne/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.nyne
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,9 +14,17 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
+import com.example.nyne.domein.util.others.NetworkObserver
 import com.example.nyne.ui.theme.NyneTheme
+import com.starry.myne.ui.screens.settings.viewmodels.SettingsViewModel
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var networkObserver: NetworkObserver
+    lateinit var settingsViewModel: SettingsViewModel
+    private lateinit var mainViewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,22 +34,27 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    NyneTheme {
-        Greeting("Android")
+    fun checkStoragePermission(): Boolean {
+        return if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
+            if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                Log.d("MainActivity::Storage", "Permission is granted"); true
+            } else {
+                Log.d("MainActivity::Storage", "Permission is revoked")
+                ActivityCompat.requestPermissions(
+                    this, arrayOf(
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.READ_EXTERNAL_STORAGE
+                    ), 1
+                ); false
+            }
+        } else {
+            true
+        }
     }
+
 }
