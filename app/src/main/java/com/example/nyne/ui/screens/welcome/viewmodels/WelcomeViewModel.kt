ed893ed2659e66fc1eb016d161e93920320a5186
CherifiMi <cherifi154949@gmail.com>
2023-08-12 10:02:48 +0100

    .

---
+++ app/src/main/java/com/example/nyne/ui/screens/welcome/viewmodels/WelcomeViewModel.kt
@@ -0,0 +1,38 @@
+/*
+Copyright 2022 - 2023 Stɑrry Shivɑm
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+
+package com.starry.myne.ui.screens.welcome.viewmodels
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.starry.myne.others.WelcomeDataStore
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class WelcomeViewModel @Inject constructor(
+    private val welcomeDataStore: WelcomeDataStore
+) : ViewModel() {
+
+    fun saveOnBoardingState(completed: Boolean) {
+        viewModelScope.launch(Dispatchers.IO) {
+            welcomeDataStore.saveOnBoardingState(completed = completed)
+        }
+    }
+}
